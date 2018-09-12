f964c6ce871138c816165f802758c804afcaceff
Lean <leanferreira17@outlook.com.ar>
2018-09-12 14:36:37 -0300

    First commit

---
+++ src/main/java/net/leanif17/demojpaweb/DemojpawebApplication.java
@@ -0,0 +1,12 @@
+package net.leanif17.demojpaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojpawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojpawebApplication.class, args);
+	}
+}
---
+++ src/test/java/net/leanif17/demojpaweb/DemojpawebApplicationTests.java
@@ -0,0 +1,16 @@
+package net.leanif17.demojpaweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemojpawebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
